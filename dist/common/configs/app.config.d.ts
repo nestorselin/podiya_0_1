6fbb79b5e69cc34f42dc082280b0632ecd968dec
nestorselin <nestorselin11@gmail.com>
2023-05-18 19:43:35 +0300

    delete node modules and dist

--- dist/common/configs/app.config.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: (() => {
-    port: string | number;
-    host: string;
-}) & import("@nestjs/config").ConfigFactoryKeyHost<{
-    port: string | number;
-    host: string;
-}>;
-export default _default;
--- dist/common/filters/http.exception.filter.d.ts
+++
@@ -1,5 +0,0 @@
-import { ArgumentsHost, HttpException } from '@nestjs/common';
-import { GqlExceptionFilter } from '@nestjs/graphql';
-export declare class HttpExceptionFilter implements GqlExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost): HttpException;
-}
