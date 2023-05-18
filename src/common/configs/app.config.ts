6b1c8d49b82f510cfeec26ee08732ecd1a4b3c13
nestorselin <nestorselin11@gmail.com>
2023-05-18 19:57:47 +0300

    rm ValidationPipe

--- src/common/configs/app.config.ts
+++ src/common/configs/app.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: process.env.PORT ?? 3000,
+  port: process.env.PORT ?? 3005,
   host: process.env.HOST ?? 'localhost',
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      disableErrorMessages: true,
-    }),
-  );
 
-  await app.listen(3000);
+  await app.listen(3005);
 }
 bootstrap();
